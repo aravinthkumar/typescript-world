2589bf46dce900d7aa1ec81c3d469855c2513680
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-12-10 09:36:51 +0530

    initial typescript example

---
+++ app.ts
@@ -0,0 +1,13 @@
+console.log("This compile the TS file for now");
+
+function add(number1, number2){
+    if (typeof number1 === 'number' && typeof number2 === 'number'){
+console.log(typeof number1);
+    return number1 + number2
+    }
+    else{
+      throw console.error("Not a number");
+    }
+}
+const result = add(5,"2");
+console.log(result);
