cee5e61c322f8f3fc8b970cf0d699d43e7ae26f3
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-12-10 11:45:09 +0530

    added class and type inference of object

--- app.js
+++ app.js
@@ -1,12 +1,60 @@
 console.log("This compile the TS file for now");
-function add(number1, number2) {
-    if (typeof number1 === 'number' && typeof number2 === 'number') {
-        console.log(typeof number1);
-        return number1 + number2;
-    }
-    else {
-        throw console.error("Not a number");
-    }
-}
-var result = add(5, "2");
+var n1 = 5;
+var n3 = "6";
+var n4 = true;
+var n2 = 2;
+var result = add(n1, n2);
 console.log(result);
+function add(number1, number2) {
+    return number1 + number2;
+}
+;
+var Calculator = /** @class */ (function () {
+    function Calculator(params1, params2, disRes) {
+        this.parameter1 = params1;
+        this.parameter2 = params2;
+        this.displayResult = disRes;
+    }
+    Calculator.prototype.add = function () {
+        if (this.displayResult) {
+            console.log("provider print: addition = " + (this.parameter1 + this.parameter2));
+        }
+        return this.parameter1 + this.parameter2;
+    };
+    Calculator.prototype.sub = function () {
+        if (this.displayResult) {
+            console.log("provider print: subtraction = " + (this.parameter1 - this.parameter2));
+        }
+        return this.parameter1 - this.parameter2;
+    };
+    Calculator.prototype.multiply = function () {
+        if (this.displayResult) {
+            console.log("provider print: multiply = " + this.parameter1 * this.parameter2);
+        }
+        return this.parameter1 * this.parameter2;
+    };
+    Calculator.prototype.div = function () {
+        if (this.displayResult) {
+            console.log("provider print: division = " + this.parameter1 / this.parameter2);
+        }
+        return this.parameter1 / this.parameter2;
+    };
+    return Calculator;
+}());
+var p1 = 2;
+var p2 = 2;
+var calc = new Calculator(2, 2, false);
+console.log("consumer print: addition = " + calc.add());
+console.log("consumer print: addition = " + calc.sub());
+console.log("consumer print: addition = " + calc.multiply());
+console.log("consumer print: addition = " + calc.div());
+var calc2 = new Calculator(4, 2, true);
+calc2.add();
+calc2.sub();
+calc2.multiply();
+calc2.div();
+var person = {
+    name: "Aravinth",
+    age: 30
+};
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -1,7 +1,77 @@
 console.log("This compile the TS file for now");
 
+let n1 = 5;
+let n3 = "6";
+let n4 = true;
+
+const n2 = 2;
+const result = add(n1,n2);
+console.log(result);
+
+
 function add(number1: number, number2: number){
     return number1 + number2
+};
+
+class Calculator
+{
+    parameter1: number;
+    parameter2: number;
+    displayResult: boolean;
+    constructor(params1:number,params2:number,disRes){
+        this.parameter1 = params1;
+        this.parameter2 = params2;
+        this.displayResult = disRes;
+    }
+
+    add(){
+        
+        if (this.displayResult){
+            console.log("provider print: addition = "+ (this.parameter1 + this.parameter2));
+        }
+        return this.parameter1 + this.parameter2;
+    }
+    sub(){
+        if (this.displayResult){
+            console.log("provider print: subtraction = "+ (this.parameter1 - this.parameter2));
+        }
+        return this.parameter1 - this.parameter2;
+    }
+    multiply(){
+        if (this.displayResult){
+            console.log("provider print: multiply = "+ this.parameter1 * this.parameter2);
+        }
+        return this.parameter1 * this.parameter2;
+    }
+    div(){
+        if (this.displayResult){
+            console.log("provider print: division = "+ this.parameter1 / this.parameter2);
+        }
+        return this.parameter1 / this.parameter2;
+    }
+
 }
-const result = add(5,2);
-console.log(result);
+
+const p1 = 2;
+const p2 = 2;
+
+const calc = new Calculator(2,2,false);
+console.log("consumer print: addition = "+ calc.add())
+console.log("consumer print: addition = "+ calc.sub())
+console.log("consumer print: addition = "+ calc.multiply())
+console.log("consumer print: addition = "+ calc.div())
+
+const calc2 = new Calculator(4,2,true);
+calc2.add();
+calc2.sub();
+calc2.multiply();
+calc2.div();
+
+const person: {
+    name: string,
+    age: number
+} = {
+    name: "Aravinth",
+    age: 30     
+}
+console.log(person.name);
